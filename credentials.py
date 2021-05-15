4cb8ecd4b9c8293375a3bcbc768a956306f00161
fsociety-hash <dmartinez@institutmvm.cat>
2021-05-15 11:03:48 +0200

    asd

--- credentials.py
+++
@@ -1,7 +0,0 @@
-creds = {
-        'user': "dmartinez_camp",
-        'passw': "4dm1n_c4mp41gn",
-        'host': "mysql-dmartinez.alwaysdata.net",
-        'database': "dmartinez_marketing",
-        'table': "tb_video"
-    }
